76a5ada68da6df1aba4e6c075832ecd5ff2c93d5
Christian Ekrem <cekrem@pm.me>
2023-09-13 13:37:14 +0200

    prettier

--- src/app.jsx
+++ src/app.jsx
@@ -1,58 +1,80 @@
 import "./app.css";
-import { useState } from 'preact/hooks';
+import { useState } from "preact/hooks";
 
 export const App = () => {
-    const [status, setStatus] = useState("idle")
+  const [status, setStatus] = useState("idle");
 
-    const handleSubmit = (e) => {
-        setStatus("loading")
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    setStatus("loading");
+    e.preventDefault();
 
-        const form = e.target;
-        const formData = new FormData(form);
+    const form = e.target;
+    const formData = new FormData(form);
 
-        const formJson = Object.fromEntries(formData.entries());
-        console.log(formJson); // TODO: send
-        fetch("/send", {
-            method: "POST",
-            body: JSON.stringify(formJson),
-        })
-            .then( ()=> {
-                setStatus("completed")
-            }, err => {
-                console.error(err)
-                setStatus("error")
-            })
-    };
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson); // TODO: send
+    fetch("/send", {
+      method: "POST",
+      body: JSON.stringify(formJson),
+    }).then(
+      () => {
+        setStatus("completed");
+      },
+      (err) => {
+        console.error(err);
+        setStatus("error");
+      }
+    );
+  };
 
-    return (
-        <Wrapper isPreview={location.hash.includes("preview")}>
-            <form method="post" onSubmit={handleSubmit}>
-                <label>Navn:</label>
-                <input type="text" name="name" required/>
+  return (
+    <Wrapper isPreview={location.hash.includes("preview")}>
+      <form method="post" onSubmit={handleSubmit}>
+        <label>Navn:</label>
+        <input type="text" name="name" required />
 
-                <div className="spacer"/>
+        <div className="spacer" />
 
-                <label>E-post:</label>
-                <input type="email" name="email" required/>
+        <label>E-post:</label>
+        <input type="email" name="email" required />
 
-                <div className="spacer"/>
+        <div className="spacer" />
 
-                <label>Melding:</label>
-                <textarea name="message" required/>
+        <label>Melding:</label>
+        <textarea name="message" required />
 
-                <div className="spacer"/>
+        <div className="spacer" />
 
-                <div class="button-wrapper">
-                    <button type="submit" className={status === "idle" ? "active enabled" : ""}>Send</button>
-                    <button type="submit" className={status === "error" ? "active enabled" : ""}>Prøv igjen</button>
-                    <button type="button" className={status === "loading" ? "active" : ""}>Sender...</button>
-                    <button type="button" className={status === "completed" ? "active" : ""}>Takk for din henvendelse!</button>
-                </div>
-            </form>
-        </Wrapper>
-    )
-
+        <div class="button-wrapper">
+          <button
+            type="submit"
+            className={status === "idle" ? "active enabled" : ""}
+          >
+            Send
+          </button>
+          <button
+            type="submit"
+            className={status === "error" ? "active enabled" : ""}
+          >
+            Prøv igjen
+          </button>
+          <button
+            type="button"
+            className={status === "loading" ? "active" : ""}
+          >
+            Sender...
+          </button>
+          <button
+            type="button"
+            className={status === "completed" ? "active" : ""}
+          >
+            Takk for din henvendelse!
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 
-const Wrapper = ({children, isPreview}) => isPreview ? <div className="preview">{children}</div> : children
+const Wrapper = ({ children, isPreview }) =>
+  isPreview ? <div className="preview">{children}</div> : children;
